0efe521cb4ed109e6aac408165bc9023023f7145
Tobias Metzke-Bernstein <tobias.metzke@camunda.com>
2022-11-11 08:20:29 +0100

    * Adjust validation annotations from `jakarta.validation` to `javax.validation`

--- src/main/java/io/camunda/connector/MyConnectorRequest.java
+++ src/main/java/io/camunda/connector/MyConnectorRequest.java
@@ -1,9 +1,9 @@
 package io.camunda.connector;
 
 import io.camunda.connector.api.annotation.Secret;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import java.util.Objects;
 
