0994cda4034516d5b4876b43403e5a4c7d801a6c
Mathias Vandaele <mathias.vandaele@camunda.com>
2026-04-14 21:11:51 +0800

    chore(template): update to connector & camunda 8.9 (#184)

--- src/test/java/io/camunda/example/integration/TestConnectorRuntimeApplication.java
+++ src/test/java/io/camunda/example/integration/TestConnectorRuntimeApplication.java
@@ -1,17 +1,9 @@
 package io.camunda.example.integration;
 
 import io.camunda.client.annotation.Deployment;
-import io.camunda.connector.api.document.DocumentFactory;
-import io.camunda.connector.runtime.core.document.DocumentFactoryImpl;
-import io.camunda.connector.runtime.core.document.store.InMemoryDocumentStore;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootApplication
 @Deployment(resources = "classpath*:/bpmn/**/*.bpmn")
-public class TestConnectorRuntimeApplication { }
+public class TestConnectorRuntimeApplication {
+}
